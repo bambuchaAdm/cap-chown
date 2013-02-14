3a5a6ebc5a1888297a380f888ef51940c01c4d7a
Łukasz Dubiel <lukasz.dubiel@llp.pl>
2013-02-14 20:24:39 +0100

    Take some impelemtation specyfic thinks to inside

--- lib/cap-chown.rb
+++ lib/cap-chown.rb
@@ -5,11 +5,14 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc "Chown all files in release to application account"
     
     task :chown do
+      old_sudo = sudo
+      set :sudo, "sudo"
       if app_user && app_group
         run "#{sudo} chown -R #{app_user}:#{app_group} #{releases_path}"
       else
         puts "WARNING: not chowning release path, because either app_user or app_group is not set!"
       end
+      set :sudo, old_sudo
     end
   end
   after 'deploy:cleanup', 'deploy:chown'
