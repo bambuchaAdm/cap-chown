8d194721bd6d73d9cc5f26c30c0fa08df1dfc2cc
Łukasz Dubiel <lukasz.dubiel@llp.pl>
2013-02-15 14:57:13 +0100

    Add reference to deploy_to directory if app_user and app_group is not defined

--- lib/cap-chown.rb
+++ lib/cap-chown.rb
@@ -10,7 +10,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       if app_user && app_group
         run "#{sudo} chown -R #{app_user}:#{app_group} #{releases_path}"
       else
-        puts "WARNING: not chowning release path, because either app_user or app_group is not set!"
+        run "#{sudo} chown -R --reference=#{deploy_to} #{releases_path}"
       end
       set :sudo, old_sudo
     end
