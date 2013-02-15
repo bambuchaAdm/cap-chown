a5c06df05b668bc52bfbf0e4758144bab17cbdac
Łukasz Dubiel <lukasz.dubiel@llp.pl>
2013-02-15 20:36:42 +0100

    Chown all deploy_to directory.

--- lib/cap-chown.rb
+++ lib/cap-chown.rb
@@ -8,9 +8,9 @@ Capistrano::Configuration.instance(:must_exist).load do
       old_sudo = sudo
       set :sudo, "sudo"
       if exists?(:app_user) && exists?(:app_group)
-        run "#{sudo} chown -R #{app_user}:#{app_group} #{releases_path}"
+        run "#{sudo} chown -R #{app_user}:#{app_group} #{deploy_to}"
       else
-        run "#{sudo} chown -R --reference=#{deploy_to} #{releases_path}"
+        run "#{sudo} chown -R --reference=#{deploy_to} #{deploy_to}"
       end
       set :sudo, old_sudo
     end
